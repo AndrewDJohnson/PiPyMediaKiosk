6c481c98b26882777ca38cd34ab6fee636a06e45
AndrewDJohnson <ad.johnson@ntlworld.com>
2017-03-16 17:00:03 +0000

    Add files via upload

--- pipptsetup.sh
+++ pipptsetup.sh
@@ -10,7 +10,8 @@ sudo dpkg -i *.deb
 sudo apt-get --yes -f install
 
 #Remove uneeded modules to free up about 1.5GB so this would fit on a 4GB card.
-sudo apt-get --yes remove dpkg-dev epiphany-browser bluej greenfoot chromium-browser epiphany-browser-data fakeroot hdparm idle idle-python2.7 idle-python3.4 idle3 info java-common dillo claws-mail libreoffice-base libreoffice-base-core libreoffice-base-drivers libreoffice-calc libreoffice-math libreoffice-writer
+sudo apt-get --yes remove dpkg-dev epiphany-browser bluej greenfoot chromium-browser epiphany-browser-data fakeroot hdparm idle idle-python2.7 idle-python3.4 idle3 info java-common dillo claws-mail libreoffice-base libreoffice-base-core libreoffice-base-drivers libreoffice-calc libreoffice-math libreoffice-writer nodejs nodejs-legacy git-man geany-common libstdc++-4.9-dev g++ g++-4.9 debian-reference-common debian-reference-en gstreamer1.0-omx gstreamer1.0-plugins-base
+
  
 sudo apt-get --yes remove  ruby1.9.1-examples ruby2.1 rubygems-integration squeak-plugins-scratch squeak-vm supercollider supercollider-common supercollider-ide   supercollider-language supercollider-server supercollider-supernova make man-db minecraft-pi netsurf-common netsurf-gtk omxplayer patch penguinspuzzle perl-modules poppler-data powermgmt-base pulseaudio python-gobject python-pifacecommon python-pifacedigitalio python-serial python-xklavier python3-gi python3-numpy python3-picamera python3-pifacecommon python3-pifacedigital-scratch-handler python3-pifacedigitalio python3-pil python3-serial raspberrypi-artwork raspi-config realvnc-vnc-server realvnc-vnc-viewer rsync rtkit ruby1.9.1 scratch sgml-base smartsim sonic-pi tcl8.5 tk8.5 triggerhappy udisks2 wolfram-engine xml-core xpdf wolfram-engine nodered geany
 
