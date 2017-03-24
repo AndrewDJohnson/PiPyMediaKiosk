116f79382da41b8a8c6bf10870935e28bfa549bc
AndrewDJohnson <ad.johnson@ntlworld.com>
2017-03-24 10:38:00 +0000

    Add files via upload

--- runppt.sh
+++ runppt.sh
@@ -3,20 +3,16 @@ cd /home/pi
 sudo rm  -f ./media/PPT/.~*#
 slide_delay=$1
 window_name="LibreOffice*"
-advance_slide="xvkbd -window \"LibreOffice 4.3\" -delay $slide_delay -text \" \""
-echo "Delay = $slide_delay, Window Name=$window_name, File is $2"
-#soffice --nologo --norestore --show $2 &
-#cmd="soffice --nologo --norestore --show \"$2\" &"
-#echo $cmd
-#bash -c $cmd 
+
+echo "Delay = $slide_delay, Window Name=$window_name"
 echo "Waiting for PPT to load in $window_name"
-($cmd)
+
 xvkbd -text "\x2000 \y2000"
 cmd_result="?"
 while [[ ! -z  $cmd_result  ]]
 do 
     #echo "Result was $cmd_result... Waiting 5 secs"
-    sleep 1
+    sleep 3
     #cmd_result=$(xdotool search --name "LibreOffice 4.3")
      cmd_result=$(xvkbd -window $window_name -delay $slide_delay -text "\[Right]" 2>&1 | grep -m 1 "no such")
     
@@ -35,12 +31,10 @@ do
        echo "Next..."
 	fi
 
-    result2=$(xdotool search --name "LibreOffice 4.3")
-    cmd_result=$result2
-	
+    cmd_result=$(xdotool search --name "LibreOffice 4.3")
     
 done
 
 echo "PPT Unloaded now."
 killall soffice.bin
-sudo rm  -f media/PPT/.~*#
+sudo rm  -f ./media/PPT/.~*#
